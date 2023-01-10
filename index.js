84fceb9c65279b5c0542d5a9e17c26c551d40805
Di <dias.ospanov@gmail.com>
2023-01-10 19:52:32 +0200

    Yarned libraries, built main code

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello');
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const lum = process.argv[3];
+const col = process.argv[2];
+
+const color = randomColor({
+  luminosity: lum,
+  hue: col,
+});
+function colorAndLog(str) {
+  console.log(chalk.hex(color).bold(str));
+}
+colorAndLog(`##############################
+##############################
+##############################
+########              ########
+########   ${color}    ########
+########              ########
+##############################
+##############################
+##############################`);
